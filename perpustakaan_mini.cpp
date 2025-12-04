0e2f8e427d803af9480b1507dff07e9001258e1b
NurilhudaTI <muhammadnurilhuda78@gmail.com>
2025-12-04 11:33:19 +0700

    Nambahin teks Menu

--- perpustakaan_mini.cpp
+++ perpustakaan_mini.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main() {
 
-    //
+    cout << "Perpustakaan Mini" << endl;
+    cout << "1. Pinjaman" << endl;
+    cout << "2. Tambah buku" << endl;
 
     return 0;
 }
