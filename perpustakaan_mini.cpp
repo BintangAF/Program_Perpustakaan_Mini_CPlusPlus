6960cc0d62cefd8ba41b3305ddf6fbf8036d044f
BintangAF <150092835+BintangAF@users.noreply.github.com>
2025-12-04 12:02:01 +0700

    nambahin menu

--- perpustakaan_mini.cpp
+++ perpustakaan_mini.cpp
@@ -6,6 +6,8 @@ int main() {
     cout << "Perpustakaan Mini" << endl;
     cout << "1. Pinjaman" << endl;
     cout << "2. Tambah buku" << endl;
+    cout << "4. Test" << endl;
+    
 
     return 0;
 }
