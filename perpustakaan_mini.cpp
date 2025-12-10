c9bca7219eb7eba52aeca418714f209f71003205
Bintang AF <kenzixboy@gmail.com>
2025-12-10 22:23:35 +0700

    Menambah properti boleh dipinjam untuk di simpan

--- perpustakaan_mini.cpp
+++ perpustakaan_mini.cpp
@@ -203,6 +203,7 @@ void simpanDataBuku(Buku buku[], int &jumlahBuku) {
              << buku[i].tahunTerbit << "|"
              << buku[i].genre << "|"
              << buku[i].stok << "|"
+             << buku[i].bolehDipinjam
              << buku[i].dihapus << endl;
     }
 
